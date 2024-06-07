df47e68f80ddc1a3afdd69c7f6ed933f36ad5a05
khushi-gunnu17 <princejain1608@gmail.com>
2024-06-07 13:56:40 +0530

    Python

--- pracrice.py
+++ pracrice.py
@@ -2343,8 +2343,22 @@ if(x):
   print("valid")
 else:
   print("invalid")
-  '''
+  
 import re
 st=input("enter string:")
 x=re.findall("[A-Z][a-z]+",st)
 print(x)
+
+
+l=[1,2,3,4,5,6]
+a=list(filter(lambda b:b%2==0,l))
+x=list(map(lambda y:y*2,l))
+print(x)
+print(a)
+'''
+from functools import reduce
+l=[1,2,3,4,5,6]
+s=lambda a:sum(b for b in range(len(l)))
+print(s(l))
+sum1=reduce((lambda x,y:x+y),l)
+print(sum1)
