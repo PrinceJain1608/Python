2bb540247ca9a3e2132044d98dea5b9b4680adcc
khushi-gunnu17 <princejain1608@gmail.com>
2024-06-07 17:52:03 +0530

    Python

---
+++ tkinter/image.py
@@ -0,0 +1,5 @@
+import tkinter
+window=tkinter.Tk()
+icon=tkinter.PhotoImage(file="C:\\Users\\Prince Jain\\OneDrive\\Pictures\\Screenshots\\Screenshot 2023-08-26 230505.png")
+label=tkinter.Label(window,image=icon).pack()
+window.mainloop()
